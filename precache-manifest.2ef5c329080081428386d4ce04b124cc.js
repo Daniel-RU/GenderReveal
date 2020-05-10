ff7d8f0210b9270dc0094607235c1731dce39c52
Daniel Renaud Urbain <renaud-urbain.daniel@courrier.uqam.ca>
2020-05-10 14:11:39 -0400

    Updates

---
+++ precache-manifest.2ef5c329080081428386d4ce04b124cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161f825ec6fc0a37751e0a08707ebc26",
+    "url": "/GenderReveal/index.html"
+  },
+  {
+    "revision": "cb15744bf8eee45a20d3",
+    "url": "/GenderReveal/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a48029f4066979d6cdbf",
+    "url": "/GenderReveal/static/css/main.93da209f.chunk.css"
+  },
+  {
+    "revision": "cb15744bf8eee45a20d3",
+    "url": "/GenderReveal/static/js/2.b3b69d12.chunk.js"
+  },
+  {
+    "revision": "c66987184f343af94070f0b8f4824afe",
+    "url": "/GenderReveal/static/js/2.b3b69d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48029f4066979d6cdbf",
+    "url": "/GenderReveal/static/js/main.047d0a19.chunk.js"
+  },
+  {
+    "revision": "810373fc5d67ad422e1a",
+    "url": "/GenderReveal/static/js/runtime-main.57c712a0.js"
+  }
+]);
